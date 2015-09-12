e62054dbe58b9d4b8a1f90088962c6c38dbdd17b
Jackey-Sparrow <1109952026@qq.com>
2015-09-12 18:17:20 +0800

    upload animate demo

--- animate/animate-property.js
+++ animate/animate-property.js
@@ -4,7 +4,40 @@
 (function (angular) {
     'use strict';
 
-    angular.module('myApp.animate',[]);
+    angular.module('myApp.animate', [])
+
+        .animation('.my-crazy-animation', function () {
+            return {
+                enter: function (element, done) {
+                    //run the animation here and call done when the animation is complete
+                    return function (cancelled) {
+                        //this (optional) function will be called when the animation
+                        //completes or when the animation is cancelled (the cancelled
+                        //flag will be set to true if cancelled).
+                    };
+                },
+                leave: function (element, done) {
+                },
+                move: function (element, done) {
+                },
+
+                //animation that can be triggered before the class is added
+                beforeAddClass: function (element, className, done) {
+                },
+
+                //animation that can be triggered after the class is added
+                addClass: function (element, className, done) {
+                },
+
+                //animation that can be triggered before the class is removed
+                beforeRemoveClass: function (element, className, done) {
+                },
+
+                //animation that can be triggered after the class is removed
+                removeClass: function (element, className, done) {
+                }
+            };
+        });
 
 
 })(angular);
