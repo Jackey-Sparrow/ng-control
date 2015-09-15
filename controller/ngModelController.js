83c66a011371dcfced7f07721b499c94c1b9a283
Jackey.Sparrow <1109952026@qq.com>
2015-09-16 00:00:25 +0800

    ngModelController

---
+++ controller/ngModelController.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Jackey Li on 2015/9/15.
+ */
+(function (angular) {
+    'use strict'
+
+    angular.module('myApp.controllers', []);
+
+    /**
+     * ngModelController
+     *
+     * $viewValue
+     * $modelValue
+     * $parsers
+     * $formatters
+     * $viewChangeListeners
+     * $error
+     * $pristine
+     * $digest
+     * $valid
+     * $invalid
+     */
+})(angular);
