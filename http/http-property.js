aff25653ebeefc45876de071fd5fb7fe760c4d10
Jackey.Sparrow <1109952026@qq.com>
2015-09-17 22:09:02 +0800

    add http file

--- http/http-property.js
+++ http/http-property.js
@@ -1,3 +1,14 @@
 /**
  * Created by Jackey Li on 2015/9/16.
  */
+(function (angular) {
+    'use strict';
+
+    angular.module('myApp.httpModule', [])
+        .factory('httpService',
+        ['$http', function ($http) {
+            var service = {};
+
+            return service;
+        }]);
+})(angular);
