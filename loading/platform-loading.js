a9adf1ddca9d557499eb49b0f47f8c20f3899d62
Jackey.Sparrow <1109952026@qq.com>
2015-12-12 22:31:33 +0800

    delete comment

--- loading/platform-loading.js
+++ loading/platform-loading.js
@@ -4,23 +4,12 @@
 (function (angular) {
 	'use strict';
 
-
-
 	//todo: height is not right when body's height is too long
 	angular.module('platform').directive('platformLoading',
 		[function () {
 			return {
 				restrict: 'EA',
 				replace: true,
-//				template: '<div class="loading-container">' +
-//				'<div class="loading-overlay"></div>' +
-//				'<div class="loading-body">' +
-//				'<div data-platform-spinner></div>' +
-//				'</div>' +
-//				'<div class="loading-close">' +
-//				'<div class="loading-close-text" data-ng-click="close()">x</div>' +
-//				'</div>' +
-//				'</div>',
 				templateUrl: 'app/components/loading/templates/loading.html',
 				link: function (scope) {
 					scope.close = scope.cancel;
@@ -32,13 +21,6 @@
 		return {
 			restrict: 'AE',
 			replace: true,
-//			template: '<div class="loading-spinner">' +
-//			'<div class="rect1"></div>' +
-//			'<div class="rect2"></div>' +
-//			'<div class="rect3"></div>' +
-//			'<div class="rect4"></div>' +
-//			'<div class="rect5"></div>' +
-//			'</div>'
 			templateUrl: 'app/components/loading/templates/spinner.html'
 		};
 	}]);
@@ -57,7 +39,6 @@
 					 };
 
 					 scope.cancel = function () {
-
 						 $document[0].body.removeChild(scope.element[0]);
 						 scope.$destroy();
 					 };
