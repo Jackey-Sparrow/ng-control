195f57f7b112943e669a88157f78e36992765754
Jackey.Sparrow <1109952026@qq.com>
2015-12-12 23:44:53 +0800

    format code

--- scrollToTop/platform-scroll-to-top.js
+++ scrollToTop/platform-scroll-to-top.js
@@ -2,36 +2,35 @@
  * Created by Jackey Li on 15/12/8.
  */
 (function (angular) {
-	'use strict';
+    'use strict';
 
-	angular.module(globals.appName).directive('platformScrollToTop',
-		['platformBrowserTeller',
-		 function (platformBrowserTeller) {
-			 return {
-				 restrict: 'EA',
-				 scope: {
-					 content: '@'
-				 },
-				 template: '<div class="scrollToTop">{{::content}}</div>',
-				 link: function (scope, element) {
+    angular.module(globals.appName).directive('platformScrollToTop',
+        ['platformBrowserTeller',
+            function (platformBrowserTeller) {
+                return {
+                    restrict: 'EA',
+                    scope: {
+                        content: '@'
+                    },
+                    template: '<div class="scrollToTop">{{::content}}</div>',
+                    link: function (scope, element) {
 
-					 element.bind('click', function () {
+                        element.bind('click', function () {
 
-						 if (platformBrowserTeller && (platformBrowserTeller.mozilla || platformBrowserTeller.msie)) {
-							 //firefox IE
-							 document.documentElement.scrollTop = 0;
-						 }
-						 else {
-							 //chrome safari
-							 document.body.scrollTop = 0;
-						 }
+                            if (platformBrowserTeller && (platformBrowserTeller.mozilla || platformBrowserTeller.msie)) {
+                                //firefox IE
+                                document.documentElement.scrollTop = 0;
+                            }
+                            else {
+                                //chrome safari
+                                document.body.scrollTop = 0;
+                            }
+                        });
 
-					 });
-
-					 scope.$on('$destroy', function () {
-						 element.unbind('click');
-					 });
-				 }
-			 };
-		 }]);
+                        scope.$on('$destroy', function () {
+                            element.unbind('click');
+                        });
+                    }
+                };
+            }]);
 })(angular);
