89dbf4fe61c3fb4bd998429a57baa86f25f8e980
Jackey.Sparrow <1109952026@qq.com>
2015-12-12 23:39:23 +0800

    format code

--- loading/platform-loading.js
+++ loading/platform-loading.js
@@ -2,55 +2,56 @@
  * Created by Jackey Li on 2015/12/9.
  */
 (function (angular) {
-	'use strict';
+    'use strict';
 
 //	var prefix = 'app/components/';
-	var prefix = '';
+    var prefix = '';
 
-	angular.module(globals.appName).directive('platformLoading',
-		[function () {
-			return {
-				restrict: 'EA',
-				replace: true,
-				templateUrl: prefix + 'loading/templates/loading.html',
-				link: function (scope) {
-					scope.close = scope.cancel;
-				}
-			};
-		}]);
+    angular.module(globals.appName).directive('platformLoading',
+        [function () {
+            return {
+                restrict: 'EA',
+                replace: true,
+                templateUrl: prefix + 'loading/templates/loading.html',
+                link: function (scope) {
+                    scope.close = scope.cancel;
+                }
+            };
+        }]);
 
-	angular.module(globals.appName).directive('platformSpinner', [function () {
-		return {
-			restrict: 'AE',
-			replace: true,
-			templateUrl: prefix + 'loading/templates/spinner.html'
-		};
-	}]);
+    angular.module(globals.appName).directive('platformSpinner', [function () {
+        return {
+            restrict: 'AE',
+            replace: true,
+            templateUrl: prefix + 'loading/templates/spinner.html'
+        };
+    }]);
 
-	angular.module(globals.appName).factory('$platformLoading',
-		['$rootScope', '$compile', '$document',
-		 function ($rootScope, $compile, $document) {
-			 return {
-				 
-				 show: function () {
+    angular.module(globals.appName).factory('$platformLoading',
+        ['$rootScope', '$compile', '$document',
+            function ($rootScope, $compile, $document) {
+                return {
 
-					 var scope = $rootScope.$new(true);
+                    
+                    show: function () {
 
-					 scope.showLoading = function () {
-						 scope.element = $compile('<platform-loading></platform-loading>')(scope);
-						 $document[0].body.appendChild(scope.element[0]);
-					 };
+                        var scope = $rootScope.$new(true);
 
-					 scope.cancel = function () {
-						 $document[0].body.removeChild(scope.element[0]);
-						 scope.$destroy();
-					 };
+                        scope.showLoading = function () {
+                            scope.element = $compile('<platform-loading></platform-loading>')(scope);
+                            $document[0].body.appendChild(scope.element[0]);
+                        };
 
-					 scope.showLoading();
+                        scope.cancel = function () {
+                            $document[0].body.removeChild(scope.element[0]);
+                            scope.$destroy();
+                        };
 
-					 return scope.cancel;
-				 }
-			 };
-		 }]);
+                        scope.showLoading();
+
+                        return scope.cancel;
+                    }
+                };
+            }]);
 
 })(angular);
